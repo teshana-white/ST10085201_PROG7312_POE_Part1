814713a4dd28b70b3907d1519bd018a14014add4
teshana-white <102995787+teshana-white@users.noreply.github.com>
2024-09-12 13:59:21 +0200

    Add files via upload

---
+++ ST10085201_PROG7312_POE_Part1/MainMenuForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace ST10085201_PROG7312_POE_Part1
+{
+    partial class MainMenuForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnReportIssues;
+        private System.Windows.Forms.Button btnLocalEvents;
+        private System.Windows.Forms.Button btnServiceStatus;
+        private System.Windows.Forms.Button btnExit;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnReportIssues = new System.Windows.Forms.Button();
+            this.btnLocalEvents = new System.Windows.Forms.Button();
+            this.btnServiceStatus = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReportIssues
+            // 
+            this.btnReportIssues.Location = new System.Drawing.Point(68, 27);
+            this.btnReportIssues.Name = "btnReportIssues";
+            this.btnReportIssues.Size = new System.Drawing.Size(135, 37);
+            this.btnReportIssues.TabIndex = 0;
+            this.btnReportIssues.Text = "Report Issues";
+            this.btnReportIssues.UseVisualStyleBackColor = true;
+            this.btnReportIssues.Click += new System.EventHandler(this.btnReportIssues_Click_1);
+            // 
+            // btnLocalEvents
+            // 
+            this.btnLocalEvents.Location = new System.Drawing.Point(68, 70);
+            this.btnLocalEvents.Name = "btnLocalEvents";
+            this.btnLocalEvents.Size = new System.Drawing.Size(135, 37);
+            this.btnLocalEvents.TabIndex = 1;
+            this.btnLocalEvents.Text = "Local Events";
+            this.btnLocalEvents.UseVisualStyleBackColor = true;
+            // 
+            // btnServiceStatus
+            // 
+            this.btnServiceStatus.Location = new System.Drawing.Point(68, 113);
+            this.btnServiceStatus.Name = "btnServiceStatus";
+            this.btnServiceStatus.Size = new System.Drawing.Size(135, 37);
+            this.btnServiceStatus.TabIndex = 2;
+            this.btnServiceStatus.Text = "Service Status";
+            this.btnServiceStatus.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(68, 156);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(135, 37);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // MainMenuForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnServiceStatus);
+            this.Controls.Add(this.btnLocalEvents);
+            this.Controls.Add(this.btnReportIssues);
+            this.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "MainMenuForm";
+            this.Text = "Main Menu";
+            this.Load += new System.EventHandler(this.MainMenuForm_Load_1);
+            this.ResumeLayout(false);
+
+        }
+        #endregion
+    }
+}
---
+++ ST10085201_PROG7312_POE_Part1/MainMenuForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ST10085201_PROG7312_POE_Part1
+{
+    public partial class MainMenuForm : Form
+    {
+        public MainMenuForm()
+        {
+            InitializeComponent();
+            // Disable options that are not yet implemented as stated by the question paper
+            btnLocalEvents.Enabled = false;
+            btnServiceStatus.Enabled = false;
+        }
+
+        // Opens the Report Issues Form so that user may record an issue on the application 
+        private void btnReportIssues_Click_1(object sender, EventArgs e)
+        {
+            ReportIssuesForm reportForm = new ReportIssuesForm();
+            reportForm.Show();
+            this.Hide(); // Hides the main form
+        }
+
+        private void MainMenuForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Closes the application
+            Application.Exit();
+        }
+
+        private void MainMenuForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
